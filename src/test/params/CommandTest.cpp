a93f9b18fb69ac3c14e5f88ab339552f9a902bcc
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-12-19 16:08:39 -0700

    Add unit tests for the params library.

---
+++ src/test/params/CommandTest.cpp
@@ -0,0 +1,34 @@
+#include <catch/catch.hpp>
+
+#include <experimental/optional>
+
+#include "bdrck/params/Argument.hpp"
+#include "bdrck/params/Command.hpp"
+
+TEST_CASE("Test command construction with valid defaulted arguments",
+          "[Parameters]")
+{
+	std::experimental::optional<bdrck::params::Command> command;
+	REQUIRE_NOTHROW(command.emplace(
+	        "test", "A test command.", bdrck::params::CommandFunction(),
+	        std::initializer_list<bdrck::params::Option>({}),
+	        std::vector<bdrck::params::Argument>(
+	                {bdrck::params::Argument("foo", "foo"),
+	                 bdrck::params::Argument("bar", "bar", "foobar"),
+	                 bdrck::params::Argument("baz", "baz", "barbaz")}),
+	        false));
+}
+
+TEST_CASE("Test command construction with invalid defaulted arguments",
+          "[Parameters]")
+{
+	std::experimental::optional<bdrck::params::Command> command;
+	REQUIRE_THROWS(command.emplace(
+	        "test", "A test command.", bdrck::params::CommandFunction(),
+	        std::initializer_list<bdrck::params::Option>({}),
+	        std::vector<bdrck::params::Argument>(
+	                {bdrck::params::Argument("foo", "foo"),
+	                 bdrck::params::Argument("bar", "bar", "foobar"),
+	                 bdrck::params::Argument("baz", "baz")}),
+	        false));
+}
