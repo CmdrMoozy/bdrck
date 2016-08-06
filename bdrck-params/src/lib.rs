e04e2b00432b5982fdfcf834c53e2f30a5991189
Axel Rasmussen <axel.rasmussen1@gmail.com>
2016-08-06 11:55:58 -0700

    Add a structure for capturing error information from the params crate.

--- bdrck-params/src/lib.rs
+++ bdrck-params/src/lib.rs
@@ -1,3 +1,39 @@
+use std::error::Error;
+use std::fmt;
+use std::string::String;
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    MissingOptionValue {
+        name: String,
+    },
+}
+
+#[derive(Debug)]
+pub struct ParamsError {
+    pub kind: ErrorKind,
+}
+
+impl Error for ParamsError {
+    fn description(&self) -> &str {
+        match self.kind {
+            ErrorKind::MissingOptionValue { name: ref _n } => {
+                "No default or specified value for option"
+            }
+        }
+    }
+}
+
+impl fmt::Display for ParamsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.kind {
+            ErrorKind::MissingOptionValue { name: ref n } => {
+                f.write_str(format!("No default or specified value for option '--{}'", n).as_str())
+            }
+        }
+    }
+}
+
 pub mod argument;
 pub mod command;
 pub mod option;
