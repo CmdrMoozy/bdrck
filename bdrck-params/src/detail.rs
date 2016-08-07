4ca1aeef7fa18b1e6460059ffab4449a1be32794
Axel Rasmussen <axel.rasmussen1@gmail.com>
2016-08-07 12:18:46 -0700

    Move tests into the modules themselves, to fix visibility issues.

--- bdrck-params/src/detail.rs
+++ bdrck-params/src/detail.rs
@@ -35,8 +35,7 @@ pub fn parse_command<'a, 'b, PI, CI>(mut parameters: PI, mut commands: CI) -> Op
 // Constructs maps for options and flags which contain the default values (if
 // any) for each of the given command's options. Note that all flags have a
 // default value of false.
-pub fn build_default_options(command: &Command)
-                             -> (HashMap<&String, &String>, HashMap<&String, bool>) {
+fn build_default_options(command: &Command) -> (HashMap<&String, &String>, HashMap<&String, bool>) {
     let mut options: HashMap<&String, &String> = HashMap::new();
     let mut flags: HashMap<&String, bool> = HashMap::new();
 
@@ -87,4 +86,115 @@ pub fn parse_options<'a, PI>
 
 
     return all_options_are_present(command, &options).map_or(Ok((options, flags)), |e| Err(e));
+}
+
+#[cfg(test)]
+mod test {
+    use std::string::String;
+    use std::vec::Vec;
+
+    use super::super::command::Command;
+    use super::build_default_options;
+    use super::parse_command;
+    use super::super::option::Option;
+
+    fn build_command_for_test(name: &str) -> Command {
+        return Command {
+            name: name.to_string(),
+            help: name.to_string(),
+            options: Vec::new(),
+            arguments: Vec::new(),
+            last_argument_is_variadic: false,
+        };
+    }
+
+    fn parse_command_works(program_parameters: &Vec<String>,
+                           commands: &Vec<Command>,
+                           expected_name: &str)
+                           -> bool {
+        return parse_command(program_parameters.iter(), commands.iter())
+            .map_or(false, |c| c.name == expected_name);
+    }
+
+    #[test]
+    fn test_parse_invalid_command() {
+        let program_parameters: Vec<String> = vec![
+    		"biff".to_string(),
+    		"foo".to_string(),
+    		"bar".to_string(),
+    		"baz".to_string(),
+    	];
+
+        let commands = vec![
+    		build_command_for_test("foo"),
+    		build_command_for_test("bar"),
+    		build_command_for_test("baz"),
+    	];
+
+        assert!(parse_command(program_parameters.iter(), commands.iter()).is_none());
+    }
+
+    #[test]
+    fn test_parse_command_no_arguments() {
+        let program_parameters: Vec<String> = vec![
+    		"bar".to_string(),
+    	];
+
+        let commands = vec![
+    		build_command_for_test("foo"),
+    		build_command_for_test("bar"),
+    		build_command_for_test("baz"),
+    	];
+
+        assert!(parse_command_works(&program_parameters, &commands, "bar"));
+    }
+
+    #[test]
+    fn test_parse_command_with_arguments() {
+        let program_parameters: Vec<String> = vec![
+    		"baz".to_string(),
+    		"foo".to_string(),
+    		"bar".to_string(),
+    		"baz".to_string(),
+    	];
+
+        let commands = vec![
+    		build_command_for_test("foo"),
+    		build_command_for_test("bar"),
+    		build_command_for_test("baz"),
+    	];
+
+        assert!(parse_command_works(&program_parameters, &commands, "baz"));
+    }
+
+    #[test]
+    fn test_build_default_options() {
+        let command = Command {
+            name: "test command".to_string(),
+            help: "test command".to_string(),
+            options: vec![
+                Option::required("a", "a", None, None),
+                Option::required("b", "b", None, Some("b")),
+                Option::required("c", "c", Some('c'), None),
+                Option::required("d", "d", Some('d'), Some("d")),
+                Option::optional("e", "e", None),
+                Option::optional("f", "f", Some('f')),
+                Option::flag("g", "g", None),
+                Option::flag("h", "h", Some('h')),
+            ],
+            arguments: Vec::new(),
+            last_argument_is_variadic: false,
+        };
+
+        let (options, flags) = build_default_options(&command);
+
+        assert!(options.len() == 2);
+        assert!(options.get(&"b".to_string()).map_or(false, |v| *v == "b"));
+        assert!(options.get(&"d".to_string()).map_or(false, |v| *v == "d"));
+
+        assert!(flags.len() == 2);
+        assert!(flags.get(&"g".to_string()).map_or(false, |v| *v == false));
+        assert!(flags.get(&"h".to_string()).map_or(false, |v| *v == false));
+    }
+
 }
--- bdrck-params/src/lib.rs
+++ bdrck-params/src/lib.rs
@@ -38,10 +38,4 @@ pub mod argument;
 pub mod command;
 pub mod option;
 
-#[cfg(test)]
-mod option_test;
-
 mod detail;
-
-#[cfg(test)]
-mod detail_test;
