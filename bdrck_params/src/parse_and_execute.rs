36246cba52d59cbb3694b73bd473e2b3c4575923
Axel Rasmussen <axel.rasmussen1@gmail.com>
2016-09-06 18:58:45 -0700

    Add a unit test for parse_and_execute_command.

--- bdrck_params/src/parse_and_execute.rs
+++ bdrck_params/src/parse_and_execute.rs
@@ -131,13 +131,61 @@ mod test {
 
     use super::EXIT_SUCCESS;
     use super::parse_and_execute;
+    use super::parse_and_execute_command;
     use super::super::argument::Argument;
     use super::super::command::Command;
     use super::super::command::ExecutableCommand;
     use super::super::option::Option;
 
     #[test]
-    fn test_parse_and_execute_command() {}
+    fn test_parse_and_execute_command() {
+        let instrumentation = FnInstrumentation::new();
+        let callback: Box<FnMut(&HashMap<&str, String>,
+                                &HashMap<&str, bool>,
+                                &HashMap<&str, Vec<String>>)> =
+            Box::new(|options, flags, arguments| {
+                instrumentation.record_call();
+
+                assert!(options.len() == 2);
+                assert!(flags.len() == 2);
+                assert!(arguments.len() == 1);
+            });
+
+        let program = "program".to_owned();
+        let parameters = vec![
+            "foobar".to_owned(),
+            "--opta=quuz".to_owned(),
+            "--flagb".to_owned(),
+            "baz".to_owned(),
+        ];
+        let commands = vec![
+            Command::new("foobar".to_owned(),
+                    "foobar".to_owned(),
+                    vec![
+                        Option::required("opta", "opta", None, None),
+                        Option::required("optb", "optb", None, Some("oof")),
+                        Option::flag("flaga", "flaga", None),
+                        Option::flag("flagb", "flagb", None),
+                    ],
+                    vec![Argument {
+                        name: "arga".to_owned(),
+                        help: "arga".to_owned(),
+                        default_value: None,
+                    }],
+                    false)
+                    .unwrap(),
+        ];
+        let mut executable_commands = vec![
+            ExecutableCommand::new(&commands[0], callback),
+        ];
+
+        assert!(instrumentation.get_call_count() == 0);
+        assert!(parse_and_execute_command(program.as_ref(),
+                                          &parameters,
+                                          &mut executable_commands) ==
+                EXIT_SUCCESS);
+        assert!(instrumentation.get_call_count() == 1);
+    }
 
     #[test]
     fn test_parse_and_execute() {
