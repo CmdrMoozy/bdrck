cd84fb5241c9d6c346de9e70939c9f570c7cb968
Axel Rasmussen <axel.rasmussen1@gmail.com>
2016-10-25 20:33:55 -0700

    Add file and line number to the log formatter.

--- bdrck_log/src/format.rs
+++ bdrck_log/src/format.rs
@@ -3,8 +3,10 @@ use log;
 use std::string::String;
 
 pub fn format_log_record(record: &log::LogRecord) -> String {
-    format!("[{}] {} - {}",
+    format!("[{} {}:{}] {} - {}",
             chrono::offset::utc::UTC::now().format("%Y-%m-%d %H:%M:%S UTC"),
+            record.location().file(),
+            record.location().line(),
             record.level(),
             record.args())
 }
