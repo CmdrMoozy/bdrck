300771b108ea4316e3d28158ab2001c87538b40d
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-01-03 20:57:47 -0800

    Re-export most common symbols in lib.rs, to simplify caller usage.

--- bdrck_params/src/lib.rs
+++ bdrck_params/src/lib.rs
@@ -11,3 +11,11 @@ pub mod option;
 pub mod parse_and_execute;
 
 mod parsed_parameters;
+
+// Re-export most commonly used symbols, to allow using this library with just
+// one "use".
+
+pub use argument::Argument;
+pub use command::{Command, CommandCallback, CommandResult, ExecutableCommand};
+pub use main_impl::{main_impl_multiple_commands, main_impl_single_command};
+pub use option::Option;
