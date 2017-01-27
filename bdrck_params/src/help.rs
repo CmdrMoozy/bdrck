21d67fd6a9a4c2033e3ade3a9afbd06f0e3f69a0
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-01-26 18:27:12 -0800

    Simplify help printing using write!().

--- bdrck_params/src/help.rs
+++ bdrck_params/src/help.rs
@@ -1,17 +1,17 @@
-use ::command::{Command, ExecutableCommand};
-use ::error::Result;
+use command::{Command, ExecutableCommand};
+use error::Result;
 use std::fmt::Write;
 
 pub fn print_program_help<'cbl, E>(f: &mut Write,
                                    program: &str,
                                    commands: &Vec<ExecutableCommand<'cbl, E>>)
                                    -> Result<()> {
-    try!(f.write_str(format!(
-        "Usage: {} command [options ...] [arguments ...]\n", program).as_ref()));
-    try!(f.write_str("Available commands:\n"));
+    write!(f,
+           "Usage: {} command [options ...] [arguments ...]\n",
+           program)?;
+    write!(f, "Available commands:\n")?;
     for command in commands.iter() {
-        try!(f.write_str(format!("\t{} - {}\n", command.command.name, command.command.help)
-            .as_ref()));
+        write!(f, "\t{} - {}\n", command.command.name, command.command.help)?;
     }
     Ok(())
 }
@@ -21,42 +21,40 @@ pub fn print_command_help(f: &mut Write,
                           command: &Command,
                           print_command_name: bool)
                           -> Result<()> {
-    try!(f.write_str(format!("Usage: {} ", program).as_ref()));
+    write!(f, "Usage: {} ", program)?;
     if print_command_name {
-        try!(f.write_str(format!("{} ", command.name).as_ref()));
+        write!(f, "{} ", command.name)?;
     }
-    try!(f.write_str("[options ...] [arguments ...]\n"));
+    write!(f, "[options ...] [arguments ...]\n")?;
 
     if !command.options.is_empty() {
-        try!(f.write_str("\nOptions:\n"));
+        write!(f, "\nOptions:\n")?;
         for option in &command.options {
-            try!(f.write_str(format!("\t--{}", option.name).as_ref()));
-            if option.short_name.is_some() {
-                try!(f.write_str(format!(", -{}", option.short_name.as_ref().unwrap()).as_ref()));
+            write!(f, "\t--{}", option.name)?;
+            if let Some(short_name) = option.short_name.as_ref() {
+                write!(f, ", -{}", short_name)?;
             }
-            try!(f.write_str(format!(" - {}", option.help).as_ref()));
+            write!(f, " - {}", option.help)?;
 
             if option.is_flag {
-                try!(f.write_str(" [Flag, default: off]"));
-            } else if option.default_value.is_some() {
-                try!(f.write_str(format!(" [Default: {}]",
-                                         option.default_value.as_ref().unwrap())
-                    .as_ref()));
+                write!(f, " [Flag, default: off]")?;
+            } else if let Some(default_value) = option.default_value.as_ref() {
+                write!(f, " [Default: {}]", default_value)?;
             }
 
-            try!(f.write_str("\n"));
+            write!(f, "\n")?;
         }
     }
 
     if !command.arguments.is_empty() {
-        try!(f.write_str("\nPositional arguments:"));
+        write!(f, "\nPositional arguments:")?;
         for argument in &command.arguments {
-            try!(f.write_fmt(format_args!("\n\t{}", argument)));
+            write!(f, "\n\t{}", argument)?;
         }
         if command.last_argument_is_variadic {
-            try!(f.write_str(" [One or more]"));
+            write!(f, " [One or more]")?;
         }
-        try!(f.write_str("\n"));
+        write!(f, "\n")?;
     }
 
     Ok(())
