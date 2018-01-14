c82e5f23e8c4c67efd453a728f28fe4a7b4388b7
Axel Rasmussen <axel.rasmussen1@gmail.com>
2018-01-13 22:41:13 -0800

    Modify the coverage script to dynamically determine executable name.

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,27 +1,50 @@
 #!/usr/bin/env bash
 
+for COMMAND in cargo grep jq kcov pgrep sed; do
+    if ! command -v "$COMMAND" > /dev/null; then
+        echo "Required command '$COMMAND' not found"
+        exit 1
+    fi
+done
+
 BASE_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
 cd "$BASE_DIR" || exit 1
-
-cargo build || exit 1
-cargo test || exit 1
-
-kcov \
-    --verify \
-    --include-path="$BASE_DIR/src" \
-    --exclude-path="$BASE_DIR/src/tests" \
-    target/cov \
-    target/debug/bdrck-d26078de399a9663 || exit 1
 
 CHROME=$(command -v google-chrome-stable || echo -n "/dev/null")
 FIREFOX=$(command -v firefox || echo -n "/dev/null")
 
-if [ -x "$CHROME" ] && pgrep -x chrome > /dev/null; then
-    echo "Opening coverage report in Chrome."
-    "$CHROME" "$BASE_DIR/target/cov/index.html" chrome://newtab/
-elif [ -x "$FIREFOX" ] && pgrep -x firefox > /dev/null; then
-    echo "Opening coverage report in Firefox."
-    "$FIREFOX" -new-tab -url "$BASE_DIR/target/cov/index.html"
-else
-    echo "Coverage report: $BASE_DIR/target/cov/index.html"
-fi
+BINARIES=$(cargo test --no-run --message-format=json 2>/dev/null | \
+    jq '.filenames' | \
+    sed -r '/^\[|\]/d' | \
+    sed -r '/(\.rlib|\.so|build-script-build)"$/d' | \
+    sed -r 's/^\s+//' | \
+    sed -r 's/^"(.*)"$/\1/')
+IFS=$'\n'
+for BINARY_PATH in "$BINARIES"; do
+    TEST_COUNT=$("$BINARY_PATH" --list | \
+        grep -P "^\d+ tests" | \
+        sed -r 's/^([0-9]+) tests.*$/\1/')
+    if [[ "$TEST_COUNT" -gt 0 ]]; then
+        BINARY=$(basename "$BINARY_PATH")
+        COVERAGE_DIR="target/cov/$BINARY"
+        COVERAGE_REPORT="$COVERAGE_DIR/index.html"
+
+        mkdir -p "$COVERAGE_DIR" || exit 1
+        kcov \
+            --verify \
+            --include-path="$BASE_DIR/src" \
+            --exclude-path="$BASE_DIR/src/tests" \
+            "$COVERAGE_DIR" \
+            "$BINARY_PATH" || exit 1
+
+        if [[ -x "$CHROME" ]] && pgrep -x $(basename "$CHROME") > /dev/null; then
+            echo "Opening coverage report in Chrome."
+            "$CHROME" "$COVERAGE_REPORT" chrome://newtab/
+        elif [[ -x "$FIREFOX" ]] && pgrep -x $(basename "$FIREFOX") > /dev/null; then
+            echo "Opening coverage report in Firefox."
+            "$FIREFOX" -new-tab -url "$COVERAGE_REPORT"
+        else
+            echo "Coverage report: $COVERAGE_REPORT"
+        fi
+    fi
+done
