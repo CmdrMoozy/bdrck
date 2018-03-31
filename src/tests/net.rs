a8d4d057c79cceeade7a204bd970f1509ff08af0
Axel Rasmussen <axel.rasmussen1@gmail.com>
2018-03-30 21:29:52 -0700

    Make ::tests::net more readable via macros!

--- src/tests/net.rs
+++ src/tests/net.rs
@@ -15,66 +15,51 @@
 use net::*;
 use std::net::IpAddr;
 
+macro_rules! ip {
+    ($e:expr) => (
+        $e.parse::<IpAddr>().unwrap()
+    )
+}
+
+macro_rules! mac {
+    ($e:expr) => (
+        $e.parse::<HardwareAddr>().unwrap()
+    )
+}
+
+macro_rules! net {
+    ($e:expr) => (
+        $e.parse::<IpNet>().unwrap()
+    )
+}
+
 #[test]
 fn test_increment_ip() {
-    assert_eq!(
-        Some("10.0.0.1".parse().unwrap()),
-        increment_ip("10.0.0.0".parse().unwrap())
-    );
-    assert_eq!(
-        Some("10.10.10.11".parse().unwrap()),
-        increment_ip("10.10.10.10".parse().unwrap())
-    );
-    assert_eq!(
-        Some("10.0.1.0".parse().unwrap()),
-        increment_ip("10.0.0.255".parse().unwrap())
-    );
-    assert_eq!(None, increment_ip("255.255.255.255".parse().unwrap()));
+    assert_eq!(Some(ip!("10.0.0.1")), increment_ip(ip!("10.0.0.0")));
+    assert_eq!(Some(ip!("10.10.10.11")), increment_ip(ip!("10.10.10.10")));
+    assert_eq!(Some(ip!("10.0.1.0")), increment_ip(ip!("10.0.0.255")));
+    assert_eq!(None, increment_ip(ip!("255.255.255.255")));
 }
 
 #[test]
 fn test_min_max_ip() {
     assert_eq!(
-        Some("10.0.0.1".parse().unwrap()),
-        min_ip("10.0.0.2".parse().unwrap(), "10.0.0.1".parse().unwrap())
+        Some(ip!("10.0.0.1")),
+        min_ip(ip!("10.0.0.2"), ip!("10.0.0.1"))
     );
     assert_eq!(
-        Some("10.0.0.2".parse().unwrap()),
-        max_ip("10.0.0.2".parse().unwrap(), "10.0.0.1".parse().unwrap())
+        Some(ip!("10.0.0.2")),
+        max_ip(ip!("10.0.0.2"), ip!("10.0.0.1"))
     );
-    assert_eq!(
-        None,
-        min_ip("10.0.0.1".parse().unwrap(), "::1".parse().unwrap())
-    );
-    assert_eq!(
-        None,
-        max_ip("10.0.0.1".parse().unwrap(), "::1".parse().unwrap())
-    );
+    assert_eq!(None, min_ip(ip!("10.0.0.1"), ip!("::1")));
+    assert_eq!(None, max_ip(ip!("10.0.0.1"), ip!("::1")));
 }
 
 #[test]
 fn test_hardware_addr_string_round_trip() {
-    assert_eq!(
-        "0c:c4:7a:7f:b6:32",
-        "0c:c4:7a:7f:b6:32"
-            .parse::<HardwareAddr>()
-            .unwrap()
-            .to_string()
-    );
-    assert_eq!(
-        "0c:c4:7a:7f:b6:32",
-        "0c-c4-7a-7f-b6-32"
-            .parse::<HardwareAddr>()
-            .unwrap()
-            .to_string()
-    );
-    assert_eq!(
-        "0c:c4:7a:7f:b6:32",
-        "0cc4.7a7f.b632"
-            .parse::<HardwareAddr>()
-            .unwrap()
-            .to_string()
-    );
+    assert_eq!("0c:c4:7a:7f:b6:32", mac!("0c:c4:7a:7f:b6:32").to_string());
+    assert_eq!("0c:c4:7a:7f:b6:32", mac!("0c-c4-7a-7f-b6-32").to_string());
+    assert_eq!("0c:c4:7a:7f:b6:32", mac!("0cc4.7a7f.b632").to_string());
 }
 
 #[test]
@@ -87,7 +72,7 @@ fn test_hardware_addr_parse_error() {
 
 #[test]
 fn test_hardware_addr_string_bit_order() {
-    let parsed: HardwareAddr = "12-34-56-78-9A-BC".parse().unwrap();
+    let parsed = mac!("12-34-56-78-9A-BC");
     assert_eq!(
         &[0x12_u8, 0x34_u8, 0x56_u8, 0x78_u8, 0x9a_u8, 0xbc_u8],
         parsed.as_bytes()
@@ -97,18 +82,9 @@ fn test_hardware_addr_string_bit_order() {
 
 #[test]
 fn test_ip_net_string_round_trip() {
-    assert_eq!(
-        "10.0.0.0/24",
-        "10.0.0.0/24".parse::<IpNet>().unwrap().to_string()
-    );
-    assert_eq!(
-        "10.0.0.0/14",
-        "10.0.0.0/14".parse::<IpNet>().unwrap().to_string()
-    );
-    assert_eq!(
-        "10.0.0.0/14",
-        "10.0.0.0/fffc0000".parse::<IpNet>().unwrap().to_string()
-    );
+    assert_eq!("10.0.0.0/24", net!("10.0.0.0/24").to_string());
+    assert_eq!("10.0.0.0/14", net!("10.0.0.0/14").to_string());
+    assert_eq!("10.0.0.0/14", net!("10.0.0.0/fffc0000").to_string());
 }
 
 #[test]
@@ -118,132 +94,81 @@ fn test_ip_net_bit_order() {
             0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
             0xff, 0x00,
         ],
-        "10.0.0.0/24".parse::<IpNet>().unwrap().get_mask()
+        net!("10.0.0.0/24").get_mask()
     );
     assert_eq!(
         [
             0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfc,
             0x00, 0x00,
         ],
-        "10.0.0.0/14".parse::<IpNet>().unwrap().get_mask()
+        net!("10.0.0.0/14").get_mask()
     );
     assert_eq!(
         [
             0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x0a, 0x37,
             0x50, 0x4e,
         ],
-        "10.0.0.0/0a37504e".parse::<IpNet>().unwrap().get_mask()
+        net!("10.0.0.0/0a37504e").get_mask()
     );
     assert_eq!(
         [
             0x4e, 0xd6, 0x2c, 0x4b, 0x1f, 0xb2, 0xbb, 0x41, 0x80, 0x80, 0x1d, 0xe9, 0xcf, 0x77,
             0xd1, 0x6e,
         ],
-        "::1/4ed62c4b1fb2bb4180801de9cf77d16e"
-            .parse::<IpNet>()
-            .unwrap()
-            .get_mask()
+        net!("::1/4ed62c4b1fb2bb4180801de9cf77d16e").get_mask()
     );
 }
 
 #[test]
 fn test_ip_net_netmask() {
-    assert_eq!(
-        "255.255.0.0".parse::<IpAddr>().unwrap(),
-        "10.10.0.0/16".parse::<IpNet>().unwrap().netmask()
-    );
-    assert_eq!(
-        "255.255.255.0".parse::<IpAddr>().unwrap(),
-        "10.10.0.0/24".parse::<IpNet>().unwrap().netmask()
-    );
-    assert_eq!(
-        "255.255.240.0".parse::<IpAddr>().unwrap(),
-        "10.10.0.0/20".parse::<IpNet>().unwrap().netmask()
-    );
+    assert_eq!(ip!("255.255.0.0"), net!("10.10.0.0/16").netmask());
+    assert_eq!(ip!("255.255.255.0"), net!("10.10.0.0/24").netmask());
+    assert_eq!(ip!("255.255.240.0"), net!("10.10.0.0/20").netmask());
 }
 
 #[test]
 fn test_ip_net_broadcast() {
-    assert_eq!(
-        "10.10.255.255".parse::<IpAddr>().unwrap(),
-        "10.10.0.0/16".parse::<IpNet>().unwrap().broadcast()
-    );
-    assert_eq!(
-        "10.10.10.255".parse::<IpAddr>().unwrap(),
-        "10.10.10.0/24".parse::<IpNet>().unwrap().broadcast()
-    );
-    assert_eq!(
-        "172.31.255.255".parse::<IpAddr>().unwrap(),
-        "172.16.0.0/12".parse::<IpNet>().unwrap().broadcast()
-    );
+    assert_eq!(ip!("10.10.255.255"), net!("10.10.0.0/16").broadcast());
+    assert_eq!(ip!("10.10.10.255"), net!("10.10.10.0/24").broadcast());
+    assert_eq!(ip!("172.31.255.255"), net!("172.16.0.0/12").broadcast());
 }
 
 #[test]
 fn test_ip_net_contains() {
     assert_eq!(
         true,
-        "10.10.10.0/24"
-            .parse::<IpNet>()
-            .unwrap()
-            .contains("10.10.10.123".parse().unwrap(), false)
+        net!("10.10.10.0/24").contains(ip!("10.10.10.123"), false)
     );
     assert_eq!(
         false,
-        "10.10.10.0/24"
-            .parse::<IpNet>()
-            .unwrap()
-            .contains("10.10.0.123".parse().unwrap(), false)
+        net!("10.10.10.0/24").contains(ip!("10.10.0.123"), false)
     );
     assert_eq!(
         true,
-        "10.10.10.0/24"
-            .parse::<IpNet>()
-            .unwrap()
-            .contains("10.10.10.0".parse().unwrap(), false)
+        net!("10.10.10.0/24").contains(ip!("10.10.10.0"), false)
     );
     assert_eq!(
         false,
-        "10.10.10.0/24"
-            .parse::<IpNet>()
-            .unwrap()
-            .contains("10.10.10.0".parse().unwrap(), true)
+        net!("10.10.10.0/24").contains(ip!("10.10.10.0"), true)
     );
     assert_eq!(
         true,
-        "10.10.10.0/24"
-            .parse::<IpNet>()
-            .unwrap()
-            .contains("10.10.10.255".parse().unwrap(), false)
+        net!("10.10.10.0/24").contains(ip!("10.10.10.255"), false)
     );
     assert_eq!(
         false,
-        "10.10.10.0/24"
-            .parse::<IpNet>()
-            .unwrap()
-            .contains("10.10.10.255".parse().unwrap(), true)
+        net!("10.10.10.0/24").contains(ip!("10.10.10.255"), true)
     );
 }
 
 #[test]
 fn test_ip_net_first_address() {
-    assert_eq!(
-        Some("10.10.10.1".parse().unwrap()),
-        "10.10.10.0/24".parse::<IpNet>().unwrap().first()
-    );
-    assert_eq!(
-        Some("10.10.0.1".parse().unwrap()),
-        "10.10.0.0/16".parse::<IpNet>().unwrap().first()
-    );
+    assert_eq!(Some(ip!("10.10.10.1")), net!("10.10.10.0/24").first());
+    assert_eq!(Some(ip!("10.10.0.1")), net!("10.10.0.0/16").first());
 }
 
 #[test]
 fn test_ip_net_last_address() {
-    assert_eq!(
-        "10.10.10.254".parse::<IpAddr>().unwrap(),
-        "10.10.10.0/24".parse::<IpNet>().unwrap().last()
-    );
-    assert_eq!(
-        "10.10.255.254".parse::<IpAddr>().unwrap(),
-        "10.10.0.0/16".parse::<IpNet>().unwrap().last()
-    );
+    assert_eq!(ip!("10.10.10.254"), net!("10.10.10.0/24").last());
+    assert_eq!(ip!("10.10.255.254"), net!("10.10.0.0/16").last());
 }
