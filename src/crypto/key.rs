dd7d0a8e70117c9bdcdba5913cc38b258e490043
Axel Rasmussen <axel.rasmussen1@gmail.com>
2018-05-29 22:26:33 -0700

    Fill out the cryptographic key API slightly.
    In particular, allow callers to construct new keys.

--- src/crypto/key.rs
+++ src/crypto/key.rs
@@ -12,17 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use error::*;
 use serde::de::{SeqAccess, Visitor};
 use serde::ser::SerializeSeq;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use sodiumoxide::crypto::hash;
+use sodiumoxide::crypto::pwhash;
 use sodiumoxide::crypto::secretbox;
+use sodiumoxide::randombytes::randombytes;
 use std::fmt;
 
 /// This module uses xsalsa20poly1305, whose nonces are 24 bytes long.
 pub const NONCE_BYTES: usize = secretbox::NONCEBYTES;
 /// This module uses sha512, which produces 64 byte digests.
 pub const DIGEST_BYTES: usize = hash::DIGESTBYTES;
+/// scryptsalsa208sha256 uses 32 byte salts.
+pub const SALT_BYTES: usize = pwhash::SALTBYTES;
 /// xsalsa20poly1305 uses 32 byte keys.
 pub const KEY_BYTES: usize = secretbox::KEYBYTES;
 
@@ -95,9 +100,66 @@ impl<'de> Deserialize<'de> for Digest {
     }
 }
 
+/// A salt is an arbitrary byte sequence which is used for password-based key
+/// derivation.
+pub struct Salt([u8; SALT_BYTES]);
+
 /// In this module's terminology, a Key is a cryptographic key of any type
 /// *which is suitable to use for encryption* (i.e., is has not been wrapped).
 pub struct Key([u8; KEY_BYTES]);
+
+impl Key {
+    /// This is a utility used to implement our various public constructors.
+    /// This constructor builds a new NormalKey from the given raw bytes.
+    fn from_bytes(data: Vec<u8>) -> Result<Self> {
+        let key = secretbox::Key::from_slice(data.as_slice());
+        if key.is_none() {
+            bail!(
+                "Invalid key; expected {} bytes, got {}",
+                KEY_BYTES,
+                data.len()
+            );
+        }
+        Ok(Key(key.unwrap().0))
+    }
+
+    /// Generate a new random key.
+    pub fn new_random() -> Result<Self> {
+        Self::from_bytes(randombytes(KEY_BYTES))
+    }
+
+    /// Derive a new key from the given password. Note that the derived key will
+    /// be different if any of the parameters to this function change, so they
+    /// need to remain fixed if you e.g. re-derive the key to decrypt some
+    /// previously-encrypted data.
+    pub fn new_password(
+        password: &[u8],
+        salt: &Salt,
+        ops_limit: usize,
+        mem_limit: usize,
+    ) -> Result<Self> {
+        let salt = pwhash::Salt::from_slice(salt.0.as_ref()).unwrap();
+        let mut key_buffer = vec![0; KEY_BYTES];
+
+        {
+            let result = pwhash::derive_key(
+                key_buffer.as_mut_slice(),
+                password,
+                &salt,
+                pwhash::OpsLimit(ops_limit),
+                pwhash::MemLimit(mem_limit),
+            );
+            if result.is_err() {
+                // NOTE: We handle this error gracefully, but in reality (by inspecting the
+                // libsodium source code) the only way this can actually fail is if the input
+                // password is *enormous*. So, this won't really fail in practice.
+                bail!("Deriving key from password failed");
+            }
+        }
+
+        Self::from_bytes(key_buffer)
+    }
+}
 
 /// A wrapped key is a Key which has been wrapped (encrypted) with another key.
 /// This is useful because it lets us have e.g. a single "master key" which is
