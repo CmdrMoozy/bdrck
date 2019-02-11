5fcf52a4f2c1699ee774b1378fcf1575c3ec0e22
Axel Rasmussen <axel.rasmussen1@gmail.com>
2019-02-10 19:45:18 -0800

    Break up the command_callback macro into smaller functions.

--- subcrates/flags-codegen/src/lib.rs
+++ subcrates/flags-codegen/src/lib.rs
@@ -18,7 +18,65 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::punctuated::Punctuated;
 use syn::token::Comma;
-use syn::{parse_macro_input, Expr, ExprVerbatim, FnArg, ItemFn, Pat, Type};
+use syn::{parse_macro_input, Expr, ExprVerbatim, FnArg, Ident, ItemFn, Pat, Type, TypeSlice};
+
+// A special case of `handle_argument`, for command callback arguments which are
+// guaranteed to have exactly one value (this includes required and boolean
+// flags).
+fn handle_required_argument(name: &Ident, ty: &Type) -> (TokenStream, Expr) {
+    (
+        TokenStream::from(quote! {
+            let #name: #ty = take_required(values.get_as(stringify!(#name))?)?;
+        }),
+        Expr::Verbatim(ExprVerbatim {
+            tts: TokenStream::from(quote! { #name }),
+        }),
+    )
+}
+
+// A special case of `handle_argument`, for command callback arguments of the
+// form `&[T]`. These should be positional arguments which can have zero or more
+// values.
+fn handle_slice_argument(name: &Ident, slice: &TypeSlice) -> (TokenStream, Expr) {
+    let elem = &slice.elem;
+    (
+        TokenStream::from(quote! {
+            let #name: Vec<#elem> = values.get_as(stringify!(#name))?;
+        }),
+        Expr::Verbatim(ExprVerbatim {
+            tts: TokenStream::from(quote! { #name.as_slice() }),
+        }),
+    )
+}
+
+// Handles a single callback function argument by generating the code for two
+// pieces (returned separately in a tuple):
+//
+// - The code which extracts the argument from the monolithic `Values` struct.
+// - The `Expr` which we'll pass in to the real command callback with the value.
+fn handle_argument(arg: &FnArg) -> (TokenStream, Expr) {
+    match arg {
+        FnArg::Captured(captured) => match &captured.pat {
+            Pat::Ident(ident) => {
+                let ty = &captured.ty;
+                let name = &ident.ident;
+
+                match ty {
+                    Type::Reference(r) => match r.elem.as_ref() {
+                        Type::Slice(s) => handle_slice_argument(name, s),
+                        _ => panic!("Command callbacks only accept references of slices."),
+                    },
+                    _ => handle_required_argument(name, ty),
+                }
+            }
+            _ => panic!("Command callback function takes an unhandled argument type."),
+        },
+        FnArg::SelfRef(_) => panic!("Command callbacks cannot be member functions."),
+        FnArg::SelfValue(_) => panic!("Command callbacks cannot be member functions."),
+        FnArg::Inferred(_) => panic!("Command callbacks cannot be lambdas with inferred captures."),
+        FnArg::Ignored(_) => panic!("Command callbacks cannot have ignored arguments."),
+    }
+}
 
 #[proc_macro_attribute]
 pub fn command_callback(
@@ -45,44 +103,9 @@ pub fn command_callback(
     let mut real_args: Punctuated<Expr, Comma> = Punctuated::new();
 
     for arg in args.iter() {
-        match arg {
-            FnArg::Captured(captured) => match &captured.pat {
-                Pat::Ident(ident) => {
-                    let ty = &captured.ty;
-                    let name = &ident.ident;
-
-                    match ty {
-                        Type::Reference(r) => match r.elem.as_ref() {
-                            Type::Slice(s) => {
-                                let elem = &s.elem;
-                                arg_parsing.extend(TokenStream::from(quote! {
-                                    let #name: Vec<#elem> = values.get_as(stringify!(#name))?;
-                                }));
-                                real_args.push(Expr::Verbatim(ExprVerbatim {
-                                    tts: TokenStream::from(quote! { #name.as_slice() }),
-                                }));
-                            }
-                            _ => panic!("Command callbacks only accept references of slices."),
-                        },
-                        _ => {
-                            arg_parsing.extend(TokenStream::from(quote! {
-                                let #name: #ty = take_required(values.get_as(stringify!(#name))?)?;
-                            }));
-                            real_args.push(Expr::Verbatim(ExprVerbatim {
-                                tts: TokenStream::from(quote! { #name }),
-                            }));
-                        }
-                    }
-                }
-                _ => panic!("Command callback function takes an unhandled argument type."),
-            },
-            FnArg::SelfRef(_) => panic!("Command callbacks cannot be member functions."),
-            FnArg::SelfValue(_) => panic!("Command callbacks cannot be member functions."),
-            FnArg::Inferred(_) => {
-                panic!("Command callbacks cannot be lambdas with inferred captures.")
-            }
-            FnArg::Ignored(_) => panic!("Command callbacks cannot have ignored arguments."),
-        }
+        let (parse, arg) = handle_argument(arg);
+        arg_parsing.extend(parse);
+        real_args.push(arg);
     }
 
     TokenStream::from(quote! {
