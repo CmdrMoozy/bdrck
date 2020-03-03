c264d4085b28ef48cb54b7aac9da28a357a5fc1b
Axel Rasmussen <axel.rasmussen1@gmail.com>
2020-03-02 22:21:04 -0800

    Refactor keystore.rs to separate "loading" and "opening".
    The idea here is, we want to be able to examine the key store's contents
    (for example, looking at wrapping key signatures) *as part of deciding
    which key we use to open it*.
    
    So, we want to do it in a several-phase process, instead of all-at-once.
    Imagine something like this:
    
    - Load the key store from disk.
    - Notice our system has a Yubikey connected.
    - Check the keystore's contents; was this Yubikey used for wrapping?
    - If yes, unwrap using it. If no, prompt for a password instead.
    
    This commit only gets us part way there. This is more to be done to
    fully enable this sort of situation, and also to "clean up" the API.

--- bdrck/src/tests/crypto/keystore.rs
+++ bdrck/src/tests/crypto/keystore.rs
@@ -27,15 +27,17 @@ fn test_keystore_save_round_trip() {
     let master_key: Option<Key>;
 
     {
-        let keystore = DiskKeyStore::open_or_new(file.path(), &wrap_key).unwrap();
-        master_key = Some(keystore.get_master_key().clone());
+        let mut keystore = DiskKeyStore::load_or_new(file.path(), &wrap_key).unwrap();
+        keystore.open(&wrap_key).unwrap();
+        master_key = Some(keystore.get_master_key().unwrap().clone());
     }
 
     {
-        let keystore = DiskKeyStore::open_or_new(file.path(), &wrap_key).unwrap();
+        let mut keystore = DiskKeyStore::load_or_new(file.path(), &wrap_key).unwrap();
+        keystore.open(&wrap_key).unwrap();
         assert_eq!(
             master_key.as_ref().unwrap().get_digest(),
-            keystore.get_master_key().get_digest()
+            keystore.get_master_key().unwrap().get_digest()
         );
     }
 }
@@ -65,16 +67,18 @@ fn test_keystore_open_with_added_key() {
     let master_key: Option<Key>;
 
     {
-        let mut keystore = DiskKeyStore::open_or_new(file.path(), &keya).unwrap();
-        master_key = Some(keystore.get_master_key().clone());
+        let mut keystore = DiskKeyStore::load_or_new(file.path(), &keya).unwrap();
+        keystore.open(&keya).unwrap();
+        master_key = Some(keystore.get_master_key().unwrap().clone());
         assert!(keystore.add_key(&keyb).unwrap());
     }
 
     {
-        let keystore = DiskKeyStore::open_or_new(file.path(), &keyb).unwrap();
+        let mut keystore = DiskKeyStore::load_or_new(file.path(), &keyb).unwrap();
+        keystore.open(&keyb).unwrap();
         assert_eq!(
             master_key.as_ref().unwrap().get_digest(),
-            keystore.get_master_key().get_digest()
+            keystore.get_master_key().unwrap().get_digest()
         );
     }
 }
@@ -87,7 +91,8 @@ fn test_add_duplicate_key() {
 
     let wrap_key = Key::new_random().unwrap();
     // Note that creating a new DiskKeyStore automatically adds the given key.
-    let mut keystore = DiskKeyStore::open_or_new(file.path(), &wrap_key).unwrap();
+    let mut keystore = DiskKeyStore::load_or_new(file.path(), &wrap_key).unwrap();
+    keystore.open(&wrap_key).unwrap();
     // Check that adding the same key again doesn't work.
     assert!(!keystore.add_key(&wrap_key).unwrap());
 }
@@ -115,7 +120,8 @@ fn test_remove_unused_key() {
     .unwrap();
     assert_ne!(keya.get_digest(), keyb.get_digest());
 
-    let mut keystore = DiskKeyStore::open_or_new(file.path(), &keya).unwrap();
+    let mut keystore = DiskKeyStore::load_or_new(file.path(), &keya).unwrap();
+    keystore.open(&keya).unwrap();
     // Test that removing some other key returns false, since it isn't in the
     // DiskKeyStore.
     assert!(!keystore.remove_key(&keyb).unwrap());
@@ -128,7 +134,8 @@ fn test_remove_only_key() {
     fs::remove_file(file.path()).unwrap();
 
     let key = Key::new_random().unwrap();
-    let mut keystore = DiskKeyStore::open_or_new(file.path(), &key).unwrap();
+    let mut keystore = DiskKeyStore::load_or_new(file.path(), &key).unwrap();
+    keystore.open(&key).unwrap();
     // Test that removing the sole key is treated as an error.
     assert!(keystore.remove_key(&key).is_err());
 }
@@ -142,7 +149,8 @@ fn test_remove_first_key() {
     let key_a = Key::new_random().unwrap();
     let key_b = Key::new_random().unwrap();
     // Create a keystore with one initial key.
-    let mut keystore = DiskKeyStore::open_or_new(file.path(), &key_a).unwrap();
+    let mut keystore = DiskKeyStore::load_or_new(file.path(), &key_a).unwrap();
+    keystore.open(&key_a).unwrap();
     // Add a second key.
     assert!(keystore.add_key(&key_b).unwrap());
     // Try removing the original key - this should succeed, since there is a
