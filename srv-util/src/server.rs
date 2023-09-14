214adf1e688dfe9735186cc0b5de2fbaefc84435
Axel Rasmussen <axel.rasmussen1@gmail.com>
2023-09-13 23:21:52 -0700

    Refactor the srv-util server API to accomodate testing.
    For example, tests want to pass in their own TcpListener, and they don't
    want to use signals for graceful shutdown.

--- srv-util/src/server.rs
+++ srv-util/src/server.rs
@@ -12,7 +12,6 @@ use std::net::{SocketAddr, TcpListener, ToSocketAddrs};
 use std::pin::Pin;
 use std::time::Duration;
 use tokio::sync::oneshot;
-use tokio::task::JoinHandle;
 use tokio_stream::wrappers::SignalStream;
 use tokio_stream::{StreamExt, StreamMap};
 use tower_http::trace::TraceLayer;
@@ -45,7 +44,7 @@ fn handle_graceful_shutdown(signals: &[SignalKind]) -> Result<Option<oneshot::Re
         return Ok(None);
     }
 
-    let (tx, rx) = tokio::sync::oneshot::channel::<c_int>();
+    let (tx, rx) = oneshot::channel::<c_int>();
 
     let mut streams: StreamMap<SignalKind, Pin<Box<SignalStream>>> = StreamMap::new();
     for kind in signals {
@@ -59,68 +58,32 @@ fn handle_graceful_shutdown(signals: &[SignalKind]) -> Result<Option<oneshot::Re
     Ok(Some(rx))
 }
 
-async fn serve_impl(
-    listener: TcpListener,
-    shutdown_rx: Option<oneshot::Receiver<c_int>>,
-    app: Router<(), Body>,
-) -> Result<()> {
-    let server =
-        Server::from_tcp(listener)?.serve(app.into_make_service_with_connect_info::<SocketAddr>());
+fn add_logging_layer(app: Router<(), Body>) -> Router<(), Body> {
+    app.layer(
+        TraceLayer::new_for_http()
+            .make_span_with(|request: &Request<_>| {
+                let remote_addr: Option<SocketAddr> = request
+                    .extensions()
+                    .get::<ConnectInfo<SocketAddr>>()
+                    .map(|ci| ci.0);
+                let real_remote_addr = request
+                    .headers()
+                    .get("x-forwarded-for")
+                    .or_else(|| request.headers().get("x-real-ip"))
+                    .map(|v| v.to_str())
+                    .transpose();
+                let referer = request
+                    .headers()
+                    .get(axum::http::header::REFERER)
+                    .map(|v| v.to_str())
+                    .transpose();
+                let user_agent = request
+                    .headers()
+                    .get(axum::http::header::USER_AGENT)
+                    .map(|v| v.to_str())
+                    .transpose();
 
-    Ok(if let Some(rx) = shutdown_rx {
-        server
-            .with_graceful_shutdown(async {
-                let signal = rx.await;
-                if cfg!(debug_assertions) {
-                    match signal {
-                        Err(e) => println!("graceful shutdown: signal handler dropped: {:?}", e),
-                        Ok(signal) => {
-                            println!("graceful shutdown: server received signal {}", signal)
-                        }
-                    }
-                }
-            })
-            .await?;
-    } else {
-        server.await?;
-    })
-}
-
-pub async fn serve<A: ToSocketAddrs>(
-    addr: A,
-    shutdown_signals: &[SignalKind],
-    add_logging_layer: bool,
-    app: Router<(), Body>,
-) -> Result<JoinHandle<Result<()>>> {
-    let listener = TcpListener::bind(addr)?;
-    let shutdown_rx = handle_graceful_shutdown(shutdown_signals)?;
-
-    let app = if add_logging_layer {
-        app.layer(
-            TraceLayer::new_for_http()
-                .make_span_with(|request: &Request<_>| {
-                    let remote_addr: Option<SocketAddr> = request
-                        .extensions()
-                        .get::<ConnectInfo<SocketAddr>>()
-                        .map(|ci| ci.0);
-                    let real_remote_addr = request
-                        .headers()
-                        .get("x-forwarded-for")
-                        .or_else(|| request.headers().get("x-real-ip"))
-                        .map(|v| v.to_str())
-                        .transpose();
-                    let referer = request
-                        .headers()
-                        .get(axum::http::header::REFERER)
-                        .map(|v| v.to_str())
-                        .transpose();
-                    let user_agent = request
-                        .headers()
-                        .get(axum::http::header::USER_AGENT)
-                        .map(|v| v.to_str())
-                        .transpose();
-
-                    info_span!(
+                info_span!(
                 "http-request",
                 remote_addr = ?remote_addr,
                 real_remote_addr = ?real_remote_addr,
@@ -129,18 +92,95 @@ pub async fn serve<A: ToSocketAddrs>(
                 version = ?request.version(),
                 referer = ?referer,
                 user_agent = ?user_agent)
-                })
-                .on_response(|response: &Response<_>, latency: Duration, _span: &Span| {
-                    debug!(
-                        "response '{}' generated in {:?}",
-                        response.status(),
-                        latency
-                    )
-                }),
-        )
+            })
+            .on_response(|response: &Response<_>, latency: Duration, _span: &Span| {
+                debug!(
+                    "response '{}' generated in {:?}",
+                    response.status(),
+                    latency
+                )
+            }),
+    )
+}
+
+pub enum GracefulShutdownKind {
+    Signal(Vec<SignalKind>),
+    Custom(oneshot::Receiver<()>),
+    None,
+}
+
+impl GracefulShutdownKind {
+    fn is_none(&self) -> bool {
+        matches!(self, GracefulShutdownKind::None)
+    }
+
+    fn get_signals(&self) -> &[SignalKind] {
+        match self {
+            GracefulShutdownKind::Signal(signals) => signals,
+            _ => &[],
+        }
+    }
+}
+
+pub async fn serve_with(
+    listener: TcpListener,
+    shutdown_kind: GracefulShutdownKind,
+    should_add_logging_layer: bool,
+    app: Router<(), Body>,
+) -> Result<()> {
+    let signal_rx = handle_graceful_shutdown(shutdown_kind.get_signals())?;
+
+    let app = if should_add_logging_layer {
+        add_logging_layer(app)
     } else {
         app
     };
 
-    Ok(tokio::spawn(serve_impl(listener, shutdown_rx, app)))
+    let server =
+        Server::from_tcp(listener)?.serve(app.into_make_service_with_connect_info::<SocketAddr>());
+
+    if !shutdown_kind.is_none() {
+        server
+            .with_graceful_shutdown(async {
+                if let Some(signal_rx) = signal_rx {
+                    let signal = signal_rx.await;
+                    if cfg!(debug_assertions) {
+                        match signal {
+                            Err(e) => {
+                                println!("graceful shutdown: signal handler dropped: {:?}", e)
+                            }
+                            Ok(signal) => {
+                                println!("graceful shutdown: server received signal {}", signal)
+                            }
+                        }
+                    }
+                } else if let GracefulShutdownKind::Custom(rx) = shutdown_kind {
+                    if let Err(e) = rx.await {
+                        if cfg!(debug_assertions) {
+                            println!("graceful shutdown: custom channel dropped: {:?}", e);
+                        }
+                    }
+                }
+            })
+            .await?;
+    } else {
+        server.await?;
+    }
+
+    Ok(())
+}
+
+pub async fn serve<A: ToSocketAddrs>(
+    addr: A,
+    shutdown_kind: GracefulShutdownKind,
+    should_add_logging_layer: bool,
+    app: Router<(), Body>,
+) -> Result<()> {
+    serve_with(
+        TcpListener::bind(addr)?,
+        shutdown_kind,
+        should_add_logging_layer,
+        app,
+    )
+    .await
 }
