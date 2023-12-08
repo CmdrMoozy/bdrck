79c73a3829d100d1111e195d374543afd1bd6a25
Axel Rasmussen <axel.rasmussen1@gmail.com>
2023-12-08 13:36:07 -0800

    Switch back to plain single-threaded tokio test macro.
    We changed this macro while debugging, but in the end this isn't needed
    for the test to pass, so switch it back to the simpler / default
    version.
    
    Since this is a test only change, no version bump is needed.

--- srv-util/src/tests/server.rs
+++ srv-util/src/tests/server.rs
@@ -56,7 +56,7 @@ async fn do_test<Fut: Future<Output = Result<()>>, F: FnOnce(Url) -> Fut>(f: F) 
     res
 }
 
-#[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+#[tokio::test]
 async fn test_hello_world() -> Result<()> {
     async fn test_impl(server_url: Url) -> Result<()> {
         let client = Client::new();
